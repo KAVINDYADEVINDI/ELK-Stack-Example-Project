be3b66ea7c070487d6fe5246c7a75395d41fbe49
Kavindya Devindi <63293156+KAVINDYADEVINDI@users.noreply.github.com>
2021-10-25 10:20:47 +0530

    initi commit

---
+++ src/test/java/com/example/elkstackapplicationexample/ElkStackApplicationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elkstackapplicationexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkStackApplicationExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
